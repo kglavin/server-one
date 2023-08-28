f55f87394edccfb0f6e3522663df27a3f1ad2f13
Kevin Glavin <kevin@bedrocksystems.com>
2023-08-28 09:45:00 -0700

    keys.debug

--- server-one/app.py
+++ server-one/app.py
@@ -70,7 +70,10 @@ def do_work_and_respond():
     ret.append("<p>" + str(datetime.now()) + "</p>")
     ret.append("<H2>trying accessing google services</H2>")
     ret.append(" GA_creds = " + os.environ['GOOGLE_APPLICATION_CREDENTIALS']) 
-    ret.append("<p>" + str(list_project_instances(creds=creds)) + "</p>")
+    with open(os.environ['GOOGLE_APPLICATION_CREDENTIALS'], 'r') as file:
+        data = file.read()
+        ret.append("<p>" + data + "</p>") 
+    #ret.append("<p>" + str(list_project_instances(creds=creds)) + "</p>")
     ret.append("<H2>trying accessing cloud run lambda function<H2>")
     #ret.append("<p>" + str(run_cloud_run()) + "</p>")
     ret.append("<H2>end of data</H2>")
