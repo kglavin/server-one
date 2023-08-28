72fc339a28ac872780c173070eb2990f6b9261ac
Kevin Glavin <kevin@bedrocksystems.com>
2023-08-28 10:54:24 -0700

    cloud-func

--- server-one/app.py
+++ server-one/app.py
@@ -7,8 +7,8 @@ import urllib
 import google.auth
 import google.auth.transport.requests
 import google.oauth2.id_token
-from google.oauth2 import service_account
-from google.auth.transport.requests import AuthorizedSession
+#from google.oauth2 import service_account
+#from google.auth.transport.requests import AuthorizedSession
 from google.oauth2.service_account import Credentials
 from googleapiclient import discovery
 import pprint
@@ -36,13 +36,13 @@ def list_project_instances(project_id = 'planar-night-391421', zone='us-west2-a'
             ret.append(f'- {instance["name"]}')
     return "<p>" + "\n".join(ret) + "</p>"
     
-def run_cloud_run():
+def run_cloud_run2():
     return "done nothing!"
     
-def run_cloud_run2():
-    req = urllib.request.Request('https://us-central1-planar-night-391421.cloudfunctions.net/function-1')
+def run_cloud_run():
+    req = urllib.request.Request('https://function-1-yvx5f5cjfq-uc.a.run.app')
     auth_req = google.auth.transport.requests.Request()
-    _id_token = google.oauth2.id_token.fetch_id_token(auth_req, 'https://us-central1-planar-night-391421.cloudfunctions.net/function-1')
+    _id_token = google.oauth2.id_token.fetch_id_token(auth_req, 'https://function-1-yvx5f5cjfq-uc.a.run.app')
     bearer = f"Bearer {_id_token}"
     print(bearer)
     req.add_header("Authorization", bearer)
@@ -51,7 +51,7 @@ def run_cloud_run2():
     if response.code != 200:
         return f'cloud-run invocation failed, response is : {response.code}'
     else:
-        return f'cloud-run invocation success, response is : {response.code}, {bearer }, {response.read()}'
+        return f'cloud-run invocation success, response is : {response.code}, {bearer}, {response.read()}'
 
 def fibonacci(n=10):
     num1 = 0
@@ -70,17 +70,9 @@ def do_work_and_respond():
     ret.append("<H1>This is a server-one pplication</H1>" )
     ret.append("<p>" + str(datetime.now()) + "</p>")
     ret.append("<H2>trying accessing google services</H2>")
-    ret.append(" GA_creds = " + os.environ['GOOGLE_APPLICATION_CREDENTIALS']) 
-    #with open(os.environ['GOOGLE_APPLICATION_CREDENTIALS'], 'r') as file:
-    #    data = file.read()
-    #    ret.append("<p>" + data + "</p>") 
-    ret.append("<p> pp creds = " + pprint.pformat(creds, indent=4) + "</p>")
-    for attr in dir(creds):
-        if not attr.startswith("__"):
-            ret.append("<p>" + str(attr) + "=" + str(getattr(creds, attr)) + "</p>")
     ret.append("<p>" + str(list_project_instances(creds=creds)) + "</p>")
     ret.append("<H2>trying accessing cloud run lambda function<H2>")
-    #ret.append("<p>" + str(run_cloud_run()) + "</p>")
+    ret.append("<p>" + str(run_cloud_run()) + "</p>")
     ret.append("<H2>end of data</H2>")
     ret.append("\n")
     return "\n".join(ret)
@@ -102,9 +94,9 @@ def onetdir():
 if __name__ == "__main__":
     #proxy = 'http://10.168.0.2:3128'
     #os.environ['https_proxy'] = proxy
-    print(os.environ['GOOGLE_APPLICATION_CREDENTIALS']) 
-    key_path = os.environ["GOOGLE_APPLICATION_CREDENTIALS"]
-    #key_path='/tmp/key.json'
+    #print(os.environ['GOOGLE_APPLICATION_CREDENTIALS']) 
+    #key_path = os.environ["GOOGLE_APPLICATION_CREDENTIALS"]
+    key_path='/tmp/key.json'
     #credentials = service_account.Credentials.from_service_account_file(key_path, scopes=['https://www.googleapis.com/auth/cloud-platform'])
     #credentials = service_account.Credentials.from_service_account_file(key_path)
     #id_token = credentials.token
