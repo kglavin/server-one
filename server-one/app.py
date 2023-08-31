b8c46f8eab09db59894cffdd46bb52c785700de4
Kevin Glavin <kevin@bedrocksystems.com>
2023-08-30 17:49:29 -0700

    debug

--- server-one/app.py
+++ server-one/app.py
@@ -68,19 +68,19 @@ def do_bigquery():
     ret.append("<H1>This is a business application running on a Bedrocked Worker Node.</H1>" )
     ret.append("<H2>" + str(datetime.now()) + "</H2>")
     ret.append("<H1>This is a call to the Google BigQuery api -  accessing census data</H1>" )
-    client = bigquery.Client()
+    #client = bigquery.Client()
     QUERY = (
         'SELECT name FROM `bigquery-public-data.usa_names.usa_1910_2013` '
         'WHERE state = "CA" '
         'LIMIT 10')
-    query_job = client.query(QUERY)
-    rows = query_job.result()
+    #query_job = client.query(QUERY)
+    #rows = query_job.result()
     ret.append(f'<H2> {QUERY} </H2><hr>')
     answer = ""
-    for row in rows:
-        answer = answer + row.name + ", "
-    #ret.append(f'<H3>{row.name}\n</H3>')
-    #ret.append(f'<H3> {str(rows)} </H3>')
+    #for row in rows:
+    #    answer = answer + row.name + ", "
+    ##ret.append(f'<H3>{row.name}\n</H3>')
+    ##ret.append(f'<H3> {str(rows)} </H3>')
     ret.append ("<H2>result = " + answer + "</H2><hr>")
     ret.append('<img src="https://storage.cloud.google.com/website-bucket-kevin/uscensus.png" alt="census">')
     ret.append('<hr><a href="http://127.0.0.1:5000/"><img src="https://storage.cloud.google.com/website-bucket-kevin/home.png" alt="home"></a>')
@@ -113,11 +113,11 @@ def do_work_and_respond(i):
         #ret.append("<H2>" + str(list_project_instances(creds=creds)) + "</H2>")
     if i == 1: 
         ret.append("<H2>Accessing Google CloudRun in US-CENTRAL Region<H2>")
-        ret.append("<H2>" + str(run_cloud_run()) + "<H2>")
+        #ret.append("<H2>" + str(run_cloud_run()) + "<H2>")
         #ret.append('<img src="https://storage.cloud.google.com/website-bucket-kevin/Flag_of_Iowa.svg.png" alt="iowa">')
     if i == 2: 
         ret.append("<H2>Accessing Google CloudRun in NORTH-EUROPE Region<H2>")
-        ret.append("<H2>" + str(run_cloud_run2()) + "</H2>")
+        #ret.append("<H2>" + str(run_cloud_run2()) + "</H2>")
         #ret.append('<img src="https://storage.cloud.google.com/website-bucket-kevin/Flag_of_Finland.svg.png" alt="finland">')
     ret.append('<hr><a href="http://127.0.0.1:5000/"><img src="https://storage.cloud.google.com/website-bucket-kevin/home.png" alt="home"></a>')
     return "\n".join(ret)
